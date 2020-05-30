e7869a2e4bea11434dde8f0dbd2a53c93981c990
Maxim Zhiburt <zhiburt@gmail.com>
2020-05-30 21:24:51 +0300

    *: init
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ shell.lua
@@ -0,0 +1,87 @@
+#!/usr/bin/lua
+
+local unistd = require 'posix.unistd'
+local posix = require 'posix'
+local wait = require 'posix.sys.wait'
+local signal = require "posix.signal"
+
+signal.signal(signal.SIGINT, function(signum)
+  io.write("\n")
+  os.exit(128 + signum)
+end)
+
+local buildin = {
+    exit = function ()
+        os.exit(0)
+    end,
+    cd = function(path)
+        if path == nil then
+            return "cd: there's no given parameter"
+        end
+
+        unistd.chdir(path)
+    end
+}
+
+local STATUS = 0;
+
+local function prep(line)
+    return string.gsub(line, "%$%?", STATUS)
+end
+
+local function start(program, ...)
+    local child, err = unistd.fork()
+    if child < 0 then return -1, err
+    elseif child == 0 then
+        local code = ({unistd.execp(program, ...)})[3]
+        os.exit(code)
+    else
+        local code = ({wait.wait(child)})[3]
+        return code
+    end
+end
+
+local function split_line(line)
+    local program = ""
+    local params = {}
+    for w in string.gmatch(line, "[^%s]+") do
+        if w ~= nil then
+            if program == "" then
+                program = w;
+            else
+                table.insert(params, w)
+            end
+        end
+    end
+
+    return program, params
+end
+
+repeat
+    io.write("> ")
+
+    local line = io.read("l")
+    if line == nil then break end
+
+    line = prep(line)
+    local program, params = split_line(line)
+    local util = buildin[program];
+    if util ~= nil then
+        local err = util(table.unpack(params))
+        if err ~= nil then
+            print(err)
+        end
+    else
+        local code, err = start(program, params)
+        if err ~= nil then
+            print("shell: "..err)
+            os.exit(-1)
+        elseif code ~= 0 then
+            local err = posix.errno(code)
+            err = string.gsub(err, "%d+$", "")
+            print(err)
+        end
+
+        STATUS = code
+    end
+until false
